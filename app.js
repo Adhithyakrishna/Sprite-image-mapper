a598cadea6949d1852d5f169e531244fa89842c4
Adhithyakrishna <adhithyakrishna@skava.com>
2017-05-13 22:25:24 +0530

    re-ordered app.js to work as expected

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 var routes = require('./routes/routes');
 var app = express();
+app.use('/static', express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 
 var hbs = exphbs.create({
@@ -11,7 +12,6 @@ var hbs = exphbs.create({
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-app.use('/static', express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
